be0ca37de963a5043ad3d6b4d630dbf5c138176d
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-03-20 17:20:57 +0700

    ft: LoginPage --Done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 type Props = {}
 
 const Login = (props: Props) => {
-  return <div>Login</div>
+  return (
+    <div>
+      <h2>Login Page</h2>
+    </div>
+  )
 }
 
 export default Login
